ceabd2ab5a57217713f62421b792dcd796145dd4
Ani Channarasappa <git@ani.dev>
2019-06-30 12:17:05 -0400

    Added two rules

--- eslint-config.js
+++ eslint-config.js
@@ -1,19 +1,21 @@
 module.exports = {
   "extends": [
-      "airbnb-base"
+    "airbnb-base"
   ],
   "rules": {
-      "function-paren-newline": ["error", "consistent"],
-      "curly": ["error", "multi"],
-      "padded-blocks": ["error", "always"],
-      "array-bracket-newline": ["error", { "minItems": 3 }],
-      "array-element-newline": ["error", { "minItems": 3 }],
-      "arrow-body-style": ["error", "as-needed"],
-      "arrow-parens": ["error", "always"],
-      "space-in-parens": ["error", "never"],
-      "indent": ["error", 2],
-      "no-multiple-empty-lines": ["error", { "max": 1 }],
-      "object-curly-newline": ["error", { "minProperties": 2 }],
-      "object-property-newline": ["error", { "allowMultiplePropertiesPerLine": true }]
+    "function-paren-newline": ["error", "consistent"],
+    "curly": ["error", "multi"],
+    "padded-blocks": ["error", "always"],
+    "array-bracket-newline": ["error", { "minItems": 3 }],
+    "array-element-newline": ["error", { "minItems": 3 }],
+    "arrow-body-style": ["error", "as-needed"],
+    "arrow-parens": ["error", "always"],
+    "space-in-parens": ["error", "never"],
+    "indent": ["error", 2],
+    "no-multiple-empty-lines": ["error", { "max": 1 }],
+    "object-curly-newline": ["error", { "minProperties": 2 }],
+    "object-property-newline": ["error", { "allowMultiplePropertiesPerLine": true }],
+    "nonblock-statement-body-position": ["error", "below"],
+    "no-underscore-dangle": ["off"]
   }
 }
